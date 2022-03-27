0bc5635cd180c6ea103cb625907e9e677f08a677
Ayeshashaikh24 <97972259+Ayeshashaikh24@users.noreply.github.com>
2022-03-27 21:34:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,6 @@
-import { render } from "enzyme"
-
 function App(){
     return(
-
-
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
     );
 }
 export default App;
