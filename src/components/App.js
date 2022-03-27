4ec38451534caad9aa1af2a112f7975b22b96e3f
Ayeshashaikh24 <97972259+Ayeshashaikh24@users.noreply.github.com>
2022-03-27 20:46:58 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,11 @@
+import { render } from "enzyme"
+
+function App(){
+    render(
+
+
 <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    );
+}
+export default App;
 
